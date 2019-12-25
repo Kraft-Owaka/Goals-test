98271e462a08ce5a58ccb0c1f4cdb0a719600d1a
kraft-owaka <owakakraft@gmail.com>
2019-12-25 11:32:55 +0300

    added a date pipe

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Goal } from '../goal';
 
+
 @Component({
   selector: 'app-goal-detail',
   templateUrl: './goal-detail.component.html',
