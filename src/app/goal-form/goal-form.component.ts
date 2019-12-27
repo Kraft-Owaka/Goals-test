ad44cd6309fdcad553b5790f7227e636793f7dbc
kraft-owaka <owakakraft@gmail.com>
2019-12-27 07:43:46 +0300

    skiped the submittion code 'code left out for reveiw due to errors'

--- src/app/goal-form/goal-form.component.ts
+++ src/app/goal-form/goal-form.component.ts
@@ -15,3 +15,25 @@ export class GoalFormComponent implements OnInit {
   }
 
 }
+/*
+import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
+
+@Component({
+  selector: 'app-goal-form',
+  templateUrl: './goal-form.component.html',
+  styleUrls: ['./goal-form.component.css']
+})
+exportclass GoalFormComponent implements OnInit {
+
+  newGoal = new Goal(0,"","",newDate());
+@Output() addGoal = new EventEmitter<Goal>();
+
+  submitGoal(){
+this.addGoal.emit(this.newGoal);
+  }
+  constructor() { }
+  ngOnInit() {
+  }
+
+}*/
