02b18b56b8d5760df2a945a92f661f35cb585338
kraft-owaka <owakakraft@gmail.com>
2019-12-27 11:00:43 +0300

    added a submit on goal-form.component.ts

--- src/app/goal-form/goal-form.component.ts
+++ src/app/goal-form/goal-form.component.ts
@@ -16,24 +16,28 @@ export class GoalFormComponent implements OnInit {
 
 }*/
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from'@angular/core';
 import { Goal } from '../goal';
+
 
 @Component({
   selector: 'app-goal-form',
   templateUrl: './goal-form.component.html',
   styleUrls: ['./goal-form.component.css']
 })
-exportclass GoalFormComponent implements OnInit {
+export class GoalFormComponent implements OnInit {
 
-  newGoal = new Goal(0,"","",newDate());
-@Output() addGoal = new EventEmitter<Goal>();
-
+  newGoal = new Goal(0,"","",new Date());
+  @Output() addGoal = new EventEmitter<Goal>();
   submitGoal(){
-this.addGoal.emit(this.newGoal);
-  }
+    this.addGoal.emit(this.newGoal);
+      }
+
   constructor() { }
+
   ngOnInit() {
   }
 
 }
+
+
