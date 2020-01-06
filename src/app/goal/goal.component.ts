407c7929083aaa36153647cb4b45f71d8b93ca8b
kraft-owaka <owakakraft@gmail.com>
2020-01-06 16:22:36 +0300

    added an injector

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -10,14 +10,18 @@ import { GoalService } from '../goal-service/goal.service';
 })
 export class GoalComponent implements OnInit {
 
-  goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2019,9,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2019,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,11,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2019,3,14)),
-  ];
+   goals: Goal[];
+
+   constructor(goalService:GoalService) {
+    this.goals= goalService.getGoals()
+}
+  //   new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2019,9,14)),
+  //   new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
+  //   new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2019,1,12)),
+  //   new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,11,18)),
+  //   new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
+  //   new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2019,3,14)),
+  // ];
 
   
   toggleDetails(index){
@@ -46,7 +50,7 @@ export class GoalComponent implements OnInit {
     }
   }
   
-  constructor() { }
+
  
   ngOnInit() {
   }
