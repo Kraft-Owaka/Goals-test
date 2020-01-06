7b5f2ba429f25322239beb4b644a38d270f8996e
kraft-owaka <owakakraft@gmail.com>
2020-01-06 21:47:33 +0300

    implemented git ignore on the url

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -20,13 +20,6 @@ export class GoalComponent implements OnInit {
     this.goals= goalService.getGoals()
     this.alertService = alertService
 }
-  //   new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2019,9,14)),
-  //   new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-  //   new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2019,1,12)),
-  //   new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,11,18)),
-  //   new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-  //   new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2019,3,14)),
-  // ];
 
   deleteGoal(isComplete, index){
     if (isComplete) {
@@ -72,6 +65,9 @@ export class GoalComponent implements OnInit {
     this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
       // Succesful API request
       this.quote = new Quote(data.author, data.quote)
+    },err=>{
+      this.quote = new Quote("Winston Churchill","Never never give up!")
+        console.log("An error occured")
     })
   }
 
